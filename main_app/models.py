c509ec4c9b26b355e2f6d4b82a43b5994ac5a7d4
Hieu <htn619@gmail.com>
2022-07-19 02:14:12 -0700

    created list with DB but list will not show up

--- main_app/models.py
+++ main_app/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    year = models.IntegerField
+    year = models.IntegerField()
 
+    def __str__(self):
+        return self.make
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Car
 # Create your views here.
-class Car:  
-  def __init__(self, make, model, year):
-    self.make = make
-    self.model = model
-    self.year = year
+#class Car:  
+#  def __init__(self, make, model, year):
+#    self.make = make
+##    self.year = year
    
 
-cars = [
-  Car('Honda', 'Civic', '1996'),
-  Car('Chevy', 'Corvette', '2012'),
-  Car('Dodge', 'Viper', '2002'),
-]
+# Car('Honda', 'Civic', '1996'),
+#  Car('Chevy', 'Corvette', '2012'),
+#  Car('Dodge', 'Viper', '2002'),
+#]
 
 def home(request):
     return HttpResponse('<h1> Hello Car-World </h1>')
@@ -21,4 +20,9 @@ def about(request):
     return render(request, 'about.html')
 
 def cars_index(request):
+    cars = Car.objects.all()
     return render(request, 'cars/index.html', { 'cars' : cars })
+
+def cars_detail(request, car_id):
+    car = Car.objects.get(id=car_id)
+    return render(request, 'cars/detail.html', {'car': car})
