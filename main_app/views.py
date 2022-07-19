f5fb1bf9e7b1a73e4774b5b88520a94536fdbec6
Hieu <htn619@gmail.com>
2022-07-19 00:07:02 -0700

    created About page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h1> Hello Car-World </h1>')
+
+def about(request):
+    return HttpResponse('This is my Car collector About page')
