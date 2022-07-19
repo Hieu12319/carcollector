a243eb9fbd437ac04387e04dc78d2c7e58c7c54c
Hieu <htn619@gmail.com>
2022-07-19 00:40:25 -0700

    created Car class

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+class Car:  
+  def __init__(self, make, model, year):
+    self.make = make
+    self.model = model
+    self.year = year
+   
+
+cars = [
+  Car('Honda', 'Civic', '1996'),
+  Car('Chevy', 'Corvette', '2012'),
+  Car('Dodge', 'Viper', '2002'),
+]
 
 def home(request):
     return HttpResponse('<h1> Hello Car-World </h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def cars_index(request):
+    return render(request, 'cats/index.html', { 'cars' : cars })
