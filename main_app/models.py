bb4b873061a8dfe97040bb6ee0f28de09f3b9095
Hieu <htn619@gmail.com>
2022-07-19 04:03:49 -0700

    created maintenance form

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,24 @@ class Car(models.Model):
     year = models.TextField()
 
     def __str__(self):
-        return self.make
+        return f"{self.make} - {self.model}"
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'car_id': self.id})
+
+MAINTAIN = (
+    ('O', 'Oil Change'),
+    ('T', 'Tire Rotation'),
+    ('S', 'Spark plug'),
+    ('V', 'Valve adjustement')
+)
+class Maintenance(models.Model):
+    date = models.DateField('maintenance date')
+    maintain = models.CharField(
+        max_length=1,
+        choices=MAINTAIN,
+        default=MAINTAIN[0][0]
+    )
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.get_maintain_display()} on {self.date}"
