08e346055ff2ee6eea78d524424aa421967e684d
Hieu <htn619@gmail.com>
2022-07-19 22:03:13 -0700

    created form to add Maintenance.

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Car
 from .forms import MaintenanceForm
 # Create your views here.
-#class Car:  
-#  def __init__(self, make, model, year):
-#    self.make = make
-##    self.year = year
-   
 
-# Car('Honda', 'Civic', '1996'),
-#  Car('Chevy', 'Corvette', '2012'),
-#  Car('Dodge', 'Viper', '2002'),
-#]
 
 def home(request):
     return HttpResponse('<h1> Hello Car-World </h1>')
@@ -30,6 +21,14 @@ def cars_detail(request, car_id):
     maintenance_form = MaintenanceForm()
     return render(request, 'cars/detail.html', {'car': car, 'maintenance_form': maintenance_form
     })
+
+def add_maintenance(request, car_id):
+    form = MaintenanceForm(request.POST)
+    if form.is_valid():
+        new_maintenance = form.save(commit=FALSE)
+        new_maintenance.car_id = car_id
+        new_maintenance.save()
+    return redirect('detail', car_id=car_id)
 
 class CarCreate(CreateView):
     model = Car
